c99f8fcbeb400b9355461d6964b143ee2e9fe392
Vanesa <vaneebg@gmail.com>
2022-04-30 00:12:42 +0200

    creado nuevo js, enlazado, puesta función pintar, hecha función random imag src y hechas cards

---
+++ se vienen cositas2.js
@@ -0,0 +1,37 @@
+let container = document.getElementById("pruebaPintada");
+console.log(container)
+
+pintarDatos()
+
+
+
+
+function pintarDatos() {
+    let usersback = JSON.parse(localStorage.getItem('usuarios'));
+
+    function getRandom(array) {
+        return array[Math.floor(Math.random() * array.length)]
+    }
+    const imgRandom = ["Assets/RANDOM/camello.jpg", "Assets/RANDOM/conejo.jpg", "Assets/RANDOM/mono.jpg", "Assets/RANDOM/perrito.jpg", "Assets/RANDOM/perro.jpg", "Assets/RANDOM/gatito.jpg"];
+
+    console.log(getRandom(imgRandom))
+
+    for (const usuario of usersback) {
+        container.innerHTML += `<div class="d-flex card" style="width: 18rem;">
+        <img src= ${getRandom(imgRandom)} alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${usuario.nombre}</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium enim sapiente quae. In laboriosam numquam maiores magnam minima delectus non, accusamus enim. Excepturi nam dolor sit obcaecati at eius reiciendis.</p>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">${usuario.email}</li>
+
+
+        </ul>
+
+    </div>
+                                
+                                `
+
+    }
+}
